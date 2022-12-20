60463036aa43aebaf1093a4ba4a4037cd36a79c3
tobiadihou <tobiadihou@gmail.com>
2022-12-20 18:46:59 +0100

    resultats Controllers 4

---
+++ resources/views/layouts/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('container')
+<h2>A propos !</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PagesController;
 /*
 |--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
+      |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -13,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[ PagesController::class, 'index']);
+
+
+Route::get('/contact-us',[PagesController::class, 'contact']);
+
+Route::get('/about-us', [PagesController::class, 'about']);
